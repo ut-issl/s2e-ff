686235ff371f16407329c96eb2ef0b39b03b5602
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-08-30 16:26:45 +0200

    Add relative position plot

---
+++ s2e-ff/scripts/Plot/plot_relative_position_rtn.py
@@ -0,0 +1,74 @@
+#
+# Plot Satellite Relative Position on RTN frame
+#
+# arg[1] : read_file_tag : time tag for default CSV output log file. ex. 220627_142946
+#
+
+#
+# Import
+#
+# plots
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+# local function
+from common import find_latest_log_tag
+# csv read
+import pandas
+# arguments
+import argparse
+
+#
+# Read Arguments
+#
+aparser = argparse.ArgumentParser()
+
+aparser.add_argument('--logs-dir', type=str, help='logs directory like "../../data/logs"', default='../../data/logs')
+aparser.add_argument('--file-tag', type=str, help='log file tag like 220627_142946')
+aparser.add_argument('--no-gui', action='store_true')
+
+args = aparser.parse_args()
+
+# log file path
+path_to_logs = args.logs_dir
+
+read_file_tag = args.file_tag
+if read_file_tag == None:
+  print("file tag does not found. use latest.")
+  read_file_tag = find_latest_log_tag(path_to_logs)
+
+print("log: " + read_file_tag)
+
+#
+# CSV file name
+#
+read_file_name  = path_to_logs + '/' + 'logs_' + read_file_tag + '/' + read_file_tag + '_default.csv'
+
+#
+# Data read and edit
+#
+# Read S2E CSV
+d1 = pandas.read_csv(read_file_name, skiprows=[1,1], sep=',', usecols=['sat1 pos from sat0_rtn(X)[m]', 'sat1 pos from sat0_rtn(Y)[m]', 'sat1 pos from sat0_rtn(Z)[m]'])
+# d2 = pandas.read_csv(read_file_name, skiprows=[1,1], sep=',', usecols=['sat2 pos from sat0_rtn(X)[m]', 'sat2 pos from sat0_rtn(Y)[m]', 'sat2 pos from sat0_rtn(Z)[m]'])
+
+#
+# Plot
+#
+fig = plt.figure(figsize=(5,5))
+ax = fig.add_subplot(111, projection='3d')
+ax.set_title("Relative Position of Satellites in RTN frame")
+ax.set_xlabel("Radial [m]")
+ax.set_ylabel("Transverse [m]")
+ax.set_zlabel("Normal [m]")
+# ax.set_xlim(-30, 30)
+# ax.set_ylim(-30, 30)
+# ax.set_zlim(-20, 20)
+
+ax.plot(0,0,0, marker="*", c="green", markersize=10)
+ax.plot(d1['sat1 pos from sat0_rtn(X)[m]'].to_numpy(),d1['sat1 pos from sat0_rtn(Y)[m]'].to_numpy(),d1['sat1 pos from sat0_rtn(Z)[m]'].to_numpy(), marker="x", c="red")
+# ax.plot(d2['sat2 pos from sat0_rtn(X)[m]'].to_numpy(),d2['sat2 pos from sat0_rtn(Y)[m]'].to_numpy(),d2['sat2 pos from sat0_rtn(Z)[m]'].to_numpy(), marker="o", c="blue")
+
+if args.no_gui:
+  plt.savefig(read_file_tag + "_relative_position_rtn.png")
+else:
+  plt.show()
