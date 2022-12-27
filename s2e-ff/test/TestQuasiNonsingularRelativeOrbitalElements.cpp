8233bb53c367ec2afbdad376c2c6fa25fe04f4fe
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-12-27 10:23:43 +0100

    Remove unnecessary variables

--- s2e-ff/test/TestQuasiNonsingularRelativeOrbitalElements.cpp
+++ s2e-ff/test/TestQuasiNonsingularRelativeOrbitalElements.cpp
@@ -27,13 +27,7 @@ TEST(QuasiNonsingularRelativeOrbitalElements, ConstructorWithPositionVelocity) {
   const double mu_m3_s2 = 3.986008e14;
   // reference satellite
   const double reference_semi_major_axis_m = 6896e3;
-  const double reference_eccentricity_x = 0.002;
-  const double reference_eccentricity_y = 0.004;
-  const double reference_inclination_rad = 1.7;
-  const double reference_raan_rad = 5.93;
-  const double reference_true_latitude_angle_rad = 0.5;
-  QuasiNonsingularOrbitalElements reference_qn_oe(reference_semi_major_axis_m, reference_eccentricity_x, reference_eccentricity_y,
-                                                  reference_inclination_rad, reference_raan_rad, reference_true_latitude_angle_rad);
+
   // target relative position and velocity
   libra::Vector<3> position_rtn_m;
   position_rtn_m[0] = -0.000213852;
@@ -44,11 +38,11 @@ TEST(QuasiNonsingularRelativeOrbitalElements, ConstructorWithPositionVelocity) {
   velocity_rtn_m_s[1] = 0.0;
   velocity_rtn_m_s[2] = 0.005298;
 
-  QuasiNonsingularRelativeOrbitalElements qn_oe(reference_semi_major_axis_m, position_rtn_m, velocity_rtn_m_s, mu_m3_s2);
+  QuasiNonsingularRelativeOrbitalElements qn_roe(reference_semi_major_axis_m, position_rtn_m, velocity_rtn_m_s, mu_m3_s2);
 
   double u = 0;
-  libra::Vector<3> calc_position_rtn_m = qn_oe.CalcRelativePositionCircularApprox_rtn_m(u);
-  libra::Vector<3> calc_velocity_rtn_m_s = qn_oe.CalcRelativeVelocityCircularApprox_rtn_m_s(u, mu_m3_s2);
+  libra::Vector<3> calc_position_rtn_m = qn_roe.CalcRelativePositionCircularApprox_rtn_m(u);
+  libra::Vector<3> calc_velocity_rtn_m_s = qn_roe.CalcRelativeVelocityCircularApprox_rtn_m_s(u, mu_m3_s2);
 
   // Compare position and velocity
   EXPECT_NEAR(calc_position_rtn_m[0], position_rtn_m[0], 1e-5);
