a697b9796592354b4600322bc9cbf6d6b02f519f
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-12-29 23:55:10 +0100

    Delete files copied to s2e-core

--- s2e-ff/src/Components/Abstract/InitializeSensorBase.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Component/Abstract/SensorBase.h>
-
-// TODO:これをコアに移して全体で共有したい
-template <size_t N>
-SensorBase<N> ReadSensorBaseInformation(const std::string file_name, const double step_width_s);
-
-#include "InitializeSensorBase_tfs.hpp"
