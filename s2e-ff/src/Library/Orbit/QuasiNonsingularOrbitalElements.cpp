95ac1d8017050ad1c9a18f4844731040c46fa962
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-01-11 11:09:11 +0100

    Fix order of elements to suit with vector expression in the text book

--- s2e-ff/src/Library/Orbit/QuasiNonsingularOrbitalElements.cpp
+++ s2e-ff/src/Library/Orbit/QuasiNonsingularOrbitalElements.cpp
@@ -10,22 +10,22 @@
 QuasiNonsingularOrbitalElements::QuasiNonsingularOrbitalElements() {
   semi_major_axis_m_ = 0.0;
   true_latitude_angle_rad_ = 0.0;
+  inclination_rad_ = 0.0;
   eccentricity_x_ = 0.0;
   eccentricity_y_ = 0.0;
-  inclination_rad_ = 0.0;
   raan_rad_ = 0.0;
   CalcOrbitParameters();
 }
 
-QuasiNonsingularOrbitalElements::QuasiNonsingularOrbitalElements(const double semi_major_axis_m, const double eccentricity_x,
-                                                                 const double eccentricity_y, const double inclination_rad, const double raan_rad,
-                                                                 const double mean_arg_latitude_rad)
+QuasiNonsingularOrbitalElements::QuasiNonsingularOrbitalElements(const double semi_major_axis_m, const double true_latitude_angle_rad,
+                                                                 const double inclination_rad, const double eccentricity_x,
+                                                                 const double eccentricity_y, const double raan_rad)
     : semi_major_axis_m_(semi_major_axis_m),
+      true_latitude_angle_rad_(true_latitude_angle_rad),
+      inclination_rad_(inclination_rad),
       eccentricity_x_(eccentricity_x),
       eccentricity_y_(eccentricity_y),
-      inclination_rad_(inclination_rad),
-      raan_rad_(raan_rad),
-      true_latitude_angle_rad_(mean_arg_latitude_rad) {
+      raan_rad_(raan_rad) {
   CalcOrbitParameters();
 }
 
@@ -82,11 +82,11 @@ QuasiNonsingularOrbitalElements ::~QuasiNonsingularOrbitalElements() {}
 
 QuasiNonsingularOrbitalElements operator-(const QuasiNonsingularOrbitalElements lhs, const QuasiNonsingularOrbitalElements rhs) {
   double semi_major_axis_m = lhs.GetSemiMajor_m() - rhs.GetSemiMajor_m();
+  double true_latitude_angle_rad = lhs.GetTrueLatAng_rad() - rhs.GetTrueLatAng_rad();
+  double inclination_rad = lhs.GetInclination_rad() - rhs.GetInclination_rad();
   double eccentricity_x = lhs.GetEccentricityX() - rhs.GetEccentricityX();
   double eccentricity_y = lhs.GetEccentricityY() - rhs.GetEccentricityY();
-  double inclination_rad = lhs.GetInclination_rad() - rhs.GetInclination_rad();
   double raan_rad = lhs.GetRaan_rad() - rhs.GetRaan_rad();
-  double true_latitude_angle_rad = lhs.GetTrueLatAng_rad() - rhs.GetTrueLatAng_rad();
 
   QuasiNonsingularOrbitalElements out(semi_major_axis_m, eccentricity_x, eccentricity_y, inclination_rad, raan_rad, true_latitude_angle_rad);
 
--- s2e-ff/src/Library/Orbit/QuasiNonsingularOrbitalElements.hpp
+++ s2e-ff/src/Library/Orbit/QuasiNonsingularOrbitalElements.hpp
@@ -24,14 +24,14 @@ class QuasiNonsingularOrbitalElements {
    * @fn QuasiNonsingularOrbitalElements
    * @brief Constructor initialized with values
    * @param[in] semi_major_axis_m: Semi major axis [m]
+   * @param[in] true_latitude_angle_rad: True latitude angle [rad]
+   * @param[in] inclination_rad: Inclination [rad]
    * @param[in] eccentricity_x: Eccentricity X component
    * @param[in] eccentricity_y: Eccentricity Y component
-   * @param[in] inclination_rad: Inclination [rad]
    * @param[in] raan_rad: Right Ascension of the Ascending Node [rad]
-   * @param[in] true_latitude_angle_rad: True latitude angle [rad]
    */
-  QuasiNonsingularOrbitalElements(const double semi_major_axis_m, const double eccentricity_x, const double eccentricity_y,
-                                  const double inclination_rad, const double raan_rad, const double true_latitude_angle_rad);
+  QuasiNonsingularOrbitalElements(const double semi_major_axis_m, const double true_latitude_angle_rad, const double inclination_rad,
+                                  const double eccentricity_x, const double eccentricity_y, const double raan_rad);
   /**
    * @fn QuasiNonsingularOrbitalElements
    * @brief Constructor initialized with position and velocity
@@ -97,11 +97,11 @@ class QuasiNonsingularOrbitalElements {
 
  private:
   double semi_major_axis_m_;        //!< Semi major axis [m]
+  double true_latitude_angle_rad_;  //!< True latitude angle (argment of periapsis + true anomaly) [rad]
+  double inclination_rad_;          //!< Inclination [rad]
   double eccentricity_x_;           //!< e * cos(arg_peri)
   double eccentricity_y_;           //!< e * sin(arg_peri)
-  double inclination_rad_;          //!< Inclination [rad]
   double raan_rad_;                 //!< Right Ascension of the Ascending Node [rad]
-  double true_latitude_angle_rad_;  //!< True latitude angle (argment of periapsis + true anomaly) [rad]
 
   // Orbit states
   double semi_latus_rectum_m_;  //!< Semi-latus rectum [m]
