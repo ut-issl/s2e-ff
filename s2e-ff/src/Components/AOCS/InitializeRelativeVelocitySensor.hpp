b9f95a0ea92aeb30295a97f3d8e5be2dcea1dfe7
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-02-16 14:38:23 +0100

    Change default value

--- s2e-ff/src/Components/AOCS/InitializeRelativeVelocitySensor.hpp
+++ s2e-ff/src/Components/AOCS/InitializeRelativeVelocitySensor.hpp
@@ -4,4 +4,4 @@
 
 RelativeVelocitySensor InitializeRelativeVelocitySensor(ClockGenerator* clock_gen, const std::string file_name, const double compo_step_time_s,
                                                         const RelativeInformation& rel_info, const Dynamics& dynamics,
-                                                        const int reference_sat_id_input = 0);
+                                                        const int reference_sat_id_input = -1);
