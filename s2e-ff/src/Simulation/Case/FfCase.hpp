8b4c1e23bacc1f38c364c4daa0656bf69d398887
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-04-12 14:39:43 +0200

    Update FfCase

--- s2e-ff/src/Simulation/Case/FfCase.hpp
+++ s2e-ff/src/Simulation/Case/FfCase.hpp
@@ -10,13 +10,22 @@ class FfCase : public SimulationCase {
   FfCase(std::string ini_fname);
   virtual ~FfCase();
 
-  void Initialize();
-  void Main();
-
   virtual std::string GetLogHeader() const;
   virtual std::string GetLogValue() const;
 
  private:
   std::vector<FfSat*> satellites_;
   RelativeInformation relative_information_;
+
+  /**
+   * @fn InitializeTargetObjects
+   * @brief Override function of InitializeTargetObjects in SimulationCase
+   */
+  void InitializeTargetObjects();
+
+  /**
+   * @fn UpdateTargetObjects
+   * @brief Override function of Main in SimulationCase
+   */
+  void UpdateTargetObjects();
 };
