e4eef4b255bedd6b32ae28bc2e767a954938aff9
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-12-25 15:01:34 +0100

    Fix small

--- s2e-ff/src/components/aocs/corner_cube_reflector.hpp
+++ s2e-ff/src/components/aocs/corner_cube_reflector.hpp
@@ -56,7 +56,7 @@ class CornerCubeReflector : public Component, public ILoggable {
 
   inline libra::Vector<3> GetReflectorPosition_i_m() const {
     libra::Vector<3> spacecraft_position_i2b_m = dynamics_->GetOrbit().GetPosition_i_m();
-    libra::Quaternion spacecraft_attitude_i2b = dynamics_->GetAttitude().GetQuaternion_i2b();
+    libra::Quaternion spacecraft_attitude_i2b = dynamics_->GetAttitude().GetQuaternion_i2b().Conjugate();
     libra::TranslationFirstDualQuaternion dual_quaternion_i2b(-spacecraft_position_i2b_m, spacecraft_attitude_i2b);
 
     libra::Vector<3> position_i_m = dual_quaternion_i2b.InverseTransformVector(libra::Vector<3>{0.0});
