5b34467bc5c079b6c8ae1567bde23412ed0da85a
Tomoki <tomoki-mochizuki@g.ecc.u-tokyo.ac.jp>
2024-01-15 11:12:40 +0900

    fix sign

--- s2e-ff/src/components/observer/relative_position_attitude_observer.cpp
+++ s2e-ff/src/components/observer/relative_position_attitude_observer.cpp
@@ -46,7 +46,7 @@ void RelativePositionAttitudeObserver::ObserveRelativePositionAttitude() {
   libra::Vector<kQpdPositioningSensorsNumber> displacement_z_axis_m{0.0};
   for (size_t qpd_id = 0; qpd_id < kQpdPositioningSensorsNumber; ++qpd_id) {
     displacement_y_axis_m[qpd_id] =
-        qpd_positioning_sensors_[qpd_id]->GetObservedYAxisDisplacementAfterCompensation_m(observed_relative_position_m_[0]);
+        -qpd_positioning_sensors_[qpd_id]->GetObservedYAxisDisplacementAfterCompensation_m(observed_relative_position_m_[0]);
     displacement_z_axis_m[qpd_id] =
         qpd_positioning_sensors_[qpd_id]->GetObservedZAxisDisplacementAfterCompensation_m(observed_relative_position_m_[0]);
   }
