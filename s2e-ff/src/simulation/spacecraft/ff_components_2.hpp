a650f41b16e7217d3c4c733d94140c9ab5297df7
Tomoki <tomoki-mochizuki@g.ecc.u-tokyo.ac.jp>
2024-02-05 17:24:13 +0900

    remove initialized_*.cpp files

--- s2e-ff/src/simulation/spacecraft/ff_components_2.hpp
+++ s2e-ff/src/simulation/spacecraft/ff_components_2.hpp
@@ -17,10 +17,10 @@
 #include <components/real/cdh/on_board_computer.hpp>
 
 #include "../../components/aocs/corner_cube_reflector.hpp"
-#include "../../components/aocs/initialize_relative_distance_sensor.hpp"
-#include "../../components/aocs/initialize_relative_position_sensor.hpp"
 #include "../../components/aocs/laser_emitter.hpp"
 #include "../../components/aocs/relative_attitude_sensor.hpp"
+#include "../../components/aocs/relative_distance_sensor.hpp"
+#include "../../components/aocs/relative_position_sensor.hpp"
 #include "../../components/ideal/initialize_relative_attitude_controller.hpp"
 
 /**
